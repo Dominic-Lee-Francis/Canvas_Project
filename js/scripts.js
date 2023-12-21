4d0ba5ff52efc9ad5886353c41422ac34507904a
Dominic Francis <dominic.l.francis@outlook.com>
2023-12-21 23:06:55 +0800

    + set-up work environment

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
+const canvas = document.querySelector('canvas'),
+ctx = canvas.getContext('2d');
 
+window.addEventListener('load', () => {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+});
+
+const drawing = (event) => {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousemove', drawing);
