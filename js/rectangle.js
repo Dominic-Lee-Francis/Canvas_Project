edf843c197ced8fc197cda06d480a0d19e2eb539
Dominic Francis <dominic.l.francis@outlook.com>
2023-12-22 20:35:04 +0800

    + draw and fill rectangle

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,3 +1,7 @@
 const drawRectangle = (event) => {
+    // if fill color is not checked, draw a rectangle border, if not then a full rectangle
+    if (!fillColor.checked) {
+        return ctx.strokeRect(event.offsetX, event.offsetY, prevMouseX - event.offsetX, prevMouseY - event.offsetY);
+    }
     ctx.fillRect(event.offsetX, event.offsetY, prevMouseX - event.offsetX, prevMouseY - event.offsetY);
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 const canvas = document.querySelector('canvas'),
 toolBtns = document.querySelectorAll('.tool'),
+fillColor = document.querySelector('#fillColor'),
 ctx = canvas.getContext('2d');
 
 // global variables and their default values
-let prevMouseX, prevMouseY,
+let prevMouseX, prevMouseY, snapshot,
 isDrawing = false,
 selectedTool = 'brush',
 brushWidth = 5;
@@ -20,16 +21,23 @@ const startDrawing = (event) => {
     prevMouseY =event.offsetY; //sets current mouse position as prevMouseY value
     ctx.beginPath(); //creates new path to draw from
     ctx.lineWidth = brushWidth; //uses brush width to change line width
+    // copies canvas data and then stores it in snapshot variable aka avoids dragging the shapes around
+    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height); 
 }
 
 const drawing = (event) => {
     if(!isDrawing) return; // if isDrawing is false, stop the function here, so only activates this function while isDrawing is true
-    
+    ctx.putImageData(snapshot, 0, 0); //add the snapshot copied canvas on to this canvas
+
     if(selectedTool === "brush") {
         ctx.lineTo(event.offsetX, event.offsetY); //creates a line using mouse
         ctx.stroke(); //adds color to the line
+        // this is just for a line
     } else if(selectedTool === "rectangle") {
         drawRectangle(event);
+        // draws rectangle, see code in rectangle.js
+    } else if(selectedTool === "circle") {
+        drawCircle(event);
     }
     
 }
