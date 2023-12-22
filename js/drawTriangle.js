942ed288c64a68b6884297af9e2727606576a220
Dominic Francis <dominic.l.francis@outlook.com>
2023-12-22 21:14:40 +0800

    + triangle.js works

--- js/drawTriangle.js
+++ js/drawTriangle.js
@@ -1,6 +1,9 @@
 const drawTriangle = (event) => {
     ctx.beginPath(); //creates new path to draw triangle
     ctx.moveTo(prevMouseX, prevMouseY); //moves triangle from mouse position
-    ctx.lineTo(event.offsetX, event.offsetY); // creates line 1
+    ctx.lineTo(event.offsetX, event.offsetY); // creates line 1 from mouse position
+    ctx.lineTo(prevMouseX * 2 - event.offsetX, event.offsetY); // creates line 2 of triangle
+    ctx.closePath(); //creates line 3 of triangle
     ctx.stroke();
+    fillColor.checked ? ctx.fill() : ctx.stroke(); //if fill color is checked, fill the triangle, if not then draw a triangle border
 }
