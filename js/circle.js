dc97cb6fe4837df1906673aeadb49fba5596286e
Dominic Francis <dominic.l.francis@outlook.com>
2023-12-22 20:53:06 +0800

    + circle fill

--- js/circle.js
+++ js/circle.js
@@ -1,6 +1,8 @@
 const drawCircle = (event) => {
+    // creates new path to draw circle
     ctx.beginPath();
+    // calculates radius and then places it in arc function. Without this the circle would not use mouse position to judge size.
     let radius = Math.sqrt(Math.pow((prevMouseX - event.offsetX), 2) + Math.pow((prevMouseY - event.offsetY), 2));
-    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
-    ctx.stroke();
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI); //circle function
+    fillColor.checked ? ctx.fill() : ctx.stroke(); //if fill color is checked, fill the circle, if not then draw a circle border
 }
